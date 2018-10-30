3ef81498eaa80dbd1829d417a19467e0aee986c9
BastienScanu <bscanu@umi.us>
2018-10-30 19:27:00 +0100

    Display the list of summaries

---
+++ src/components/SummariesList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>Summaries</h1>
+    <ul>
+      <li v-for="(value, key) in summaries" v-bind:key="key">
+        {{ key }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import foo from '../data/example_references.json';
+
+export default {
+  name: 'SummariesList',
+  data() {
+    return {
+      summaries: foo,
+    };
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import SummariesList from '@/components/SummariesList';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'SummariesList',
+      component: SummariesList,
     },
   ],
 });
