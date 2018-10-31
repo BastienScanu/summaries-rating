1ea310c9848ad8d790df21fd309d935dce9ff004
BastienScanu <bastien.scanu@gmail.com>
2018-10-31 17:42:24 +0100

    use express router

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const router = express.Router();
+require('./Routes')(router);
+app.use('/', router);
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
