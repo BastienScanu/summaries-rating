752e6bf8901370a0a143f728662f2c56ecc26245
BastienScanu <bastien.scanu@gmail.com>
2018-11-01 17:30:07 +0100

    Add user selection

--- src/client/components/SummariesList.vue
+++ src/client/components/SummariesList.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <h1>Summaries</h1>
-    <ul>
-      <li v-for="(value, key) in summaries" v-bind:key="key">
-        <router-link :to="{ name: 'Summary', params: { summaryId: key }}">{{ key }}</router-link>
-      </li>
-    </ul>
+    <div v-if="user">
+      <h1>Summaries</h1>
+      <ul>
+        <li v-for="(value, key) in summaries" v-bind:key="key">
+          <router-link :to="{ name: 'Summary', params: { summaryId: key }}">{{ key }}</router-link>
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <p>You must choose an user to start</p>
+    </div>
+
+    <select v-model="user">
+      <option disabled value="">Select your user</option>
+      <option>Aragorn</option>
+      <option>Gandalf</option>
+      <option>Gimli</option>
+    </select>
   </div>
 </template>
 
@@ -15,6 +27,7 @@ export default {
   name: 'SummariesList',
   data() {
     return {
+      user: '',
       summaries: [],
     };
   },
