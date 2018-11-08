95b4c9a16682d6af5ead6e8dbff159ec9ebb4105
BastienScanu <bastien.scanu@gmail.com>
2018-11-08 13:01:50 +0100

    coding style

--- src/server/Database.js
+++ src/server/Database.js
@@ -1,32 +1,29 @@
 const References = require('./data/example_references.json');
 const Systems = require('./data/example_systems.json');
+
 const Data = {
   reference: References,
-  system: Systems
+  system: Systems,
 };
 
 class Database {
-
   static model(model) {
     return (req, res, next) => {
-      console.log(model);
       req.model = model;
       return next();
-    }
-  };
+    };
+  }
 
   static load(req, res, next) {
     const model = req.model;
     if (Data[model] && Data[model][req.params[`${model}Id`]]) {
       req.data = Data[model][req.params[`${model}Id`]];
       return next();
-    } else {
-      return res.status(404).send({message: 'Ressource not found'});
     }
-  };
+    return res.status(404).send({message: 'Ressource not found'});
+  }
 
   static getAll(req, res) {
-    console.log('all');
     return res.status(200).json(Data[req.model]);
   }
 
